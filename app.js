537bcfe4f244f7975f51f9ea9275fa581b7400e6
Reza Aghajani <rezaaghajani@protonmail.com>
2022-03-22 18:51:38 +0430

    theme created

---
+++ app.js
@@ -0,0 +1,13 @@
+let fKey = document.getElementById("f-key");
+
+let gKey = document.getElementById("g-key");
+
+let hKey = document.getElementById("h-key");
+
+let jKey = document.getElementById("j-key");
+
+let kKey = document.getElementById("k-key");
+
+fKey.addEventListener("click", function() {
+  console.log("f key was clicked");
+})
